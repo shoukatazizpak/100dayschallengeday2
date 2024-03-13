2cdbdb7f6fec016d3608b19ee3960df48aca786e
[Shaukat Aziz] <[shoukatazizpak@gmail.com]>
2024-03-13 23:18:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var newname = "\t\n shoukat \t\n";
+console.log(newname);
+var uname = "\t\n shoukat \t\n";
+console.log(uname.trim());
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said, \u201CA person who never made a mistake never tried anything new.\u201D");
+console.log(message);
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
---
+++ main.ts
@@ -0,0 +1,13 @@
+let newname: string = "\t\n shoukat \t\n"; 
+
+console.log(newname); 
+let uname: string = "\t\n shoukat \t\n"; 
+
+console.log(uname.trim()); 
+
+let famous_person: string = "Albert Einstein";
+let message: string = `${famous_person} once said, “A person who never made a mistake never tried anything new.”`;
+
+console.log(message);
+
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
